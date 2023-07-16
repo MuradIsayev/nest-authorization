d01749a291d07f4d02b896638b0aa00e1066283a
MurAd <muradisayevazer@gmail.com>
2023-07-17 01:30:15 +0400

    Guard completed

--- src/users/roles.guard.ts
+++ src/users/roles.guard.ts
@@ -1,11 +1,30 @@
 import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
+import { Role } from './entities/role.enum';
+import { User } from './entities/user.entity';
 
 @Injectable()
 export class RolesGuard implements CanActivate {
   constructor(private reflector: Reflector) {}
 
   canActivate(context: ExecutionContext): boolean {
-    return false;
+    const requiredRoles = this.reflector.getAllAndOverride<Role[]>('roles', [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+
+    if (!requiredRoles) {
+      return true; // If no roles are required, allow access
+    }
+
+    // const { user } = context.switchToHttp().getRequest();
+
+    // Mock user
+    const user: User = {
+      name: 'John Doe',
+      roles: [Role.USER],
+    };
+
+    return requiredRoles.some((role) => user.roles?.includes(role)); // If user has any of the required roles
   }
 }
