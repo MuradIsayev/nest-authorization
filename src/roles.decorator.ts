f6d450e44a4748b5333242edb23fce6ab2c43684
MurAd <muradisayevazer@gmail.com>
2023-07-16 17:45:33 +0400

    little update

--- src/roles.decorator.ts
+++ src/roles.decorator.ts
@@ -1,4 +1,5 @@
 import { SetMetadata } from "@nestjs/common";
+import { Role } from "./users/entities/role.enum";
 
-export const Roles = (...roles: string[]) => SetMetadata('roles', roles); 
+export const Roles = (...roles: Role[]) => SetMetadata('roles', roles); 
 // This is a custom decorator that takes in a list of roles and sets the metadata for the roles key to the list of roles.
