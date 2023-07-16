7bb88fcb604098bc11f923e399c16f3e9eb8129b
MurAd <muradisayevazer@gmail.com>
2023-07-16 23:16:17 +0400

    Roles Guard registered globally

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { RolesGuard } from './users/roles.guard';
 
 @Module({
   imports: [UsersModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, { provide: 'APP_GUARD', useClass: RolesGuard }], 
 })
 export class AppModule {}
